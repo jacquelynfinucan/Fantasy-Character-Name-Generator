57b263cca2f9a4f4583f9942866d001201486543
Jacquelyn Finucan <jacquelynfinucan@gmail.com>
2021-12-27 18:08:44 -0500

    First commit of character name side project into github

---
+++ FantasyMethods.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FantasyCharacterNameGenerator
+{
+    public class FantasyMethods
+    {
+        //          0       1         2        3        4       5      6        7        8         9
+        public string[] femaleFirstNames = new string[]
+             { "Misty", "Willow", "Piper", "Silvyr", "Talia", "Aura", "Luna", "Nyssa", "Elysse", "Leafa" };
+        public string[] maleFirstNames = new string[]
+             { "Wen", "Elwin", "Caldor", "Dray", "Galdeen", "Sulfa", "Vyn", "Zorion", "Jackzl", "Temley" };
+        public string[] neutralFirstNames = new string[]
+             { "Axil", "Ragna", "Zika", "Soril", "Sterling", "Twindle",  "Dorath", "Pondra", "Laken", "Kelza"  };
+        public string[] allLastNames = new string[]
+             { "Amberhide", "Autumnbow", "Duskbloom", "Embershadow", "Feathersworn", "Slatemoore", "Skymeaden", "Winddane", "Whispercrest", "Riversforde" };
+        //          0            1            2             3               4             5             6            7             8             9
+        public string[] colorOptions = new string[]
+             { "Red", "Orange", "Yellow", "Green", "Blue", "Purple", "Pink", "White", "Black", "Multi/Tye-Dye" };
+        public string[] creatureOptions = new string[]
+             {"Unicorn", "Dragon", "Griffin", "Kitsune", "Cerberus", "Sphinx", "Chimera", "Phoenix", "Hydra", "Chupacabra"};
+        //{"Frosted Flakes", "Lucky Charms", "Cocoa Puffs", "Fruit Loops", "Captain Crunch", "Honey Bunches of Oats", "Cheerios", "Cinnamon Toast Crunch", "Shredded Wheat", "Other (I'm a unique snowflake)" };
+
+        public void PrintColorOptions()
+        {
+            for (int i = 0; i < colorOptions.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}-{colorOptions[i]}"); //numbers each as it's printed, starting at 1 (not 0 for user experience)
+            }
+            Console.WriteLine("Or you can press Q to Quit.");
+        }
+
+        public void PrintCreatureOptions()
+        {
+            for (int i = 0; i < creatureOptions.Length; i++)
+            {
+                Console.WriteLine($"{i + 1}-{creatureOptions[i]}"); //numbers each as it's printed, starting at 1 (not 0 for user experience)
+            }
+            Console.WriteLine("Or you can press Q to Quit.");
+        }
+        public string GetFirstName(string genderLetter, int colorNumber)
+        {
+            if(colorNumber == -1)
+            {
+                Console.WriteLine("Error reading color selection.");
+            }
+            else if(genderLetter == "F")
+            {
+
+            }
+            else
+            {
+
+            }
+            return "";
+        }
+
+        public string GetLastName(string genderLetter, int creatureNumber)
+        {
+            //select corresponding name from allLastNames as per user input-accessed via array index[0]
+            if (creatureNumber == -1)
+            {
+                Console.WriteLine("Error reading creature selection.");
+            }
+            //else if()
+            return "";
+        }
+
+    } //end of class
+}
---
+++ Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace FantasyCharacterNameGenerator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            FantasyMethods fantasyMethods = new FantasyMethods(); //instance of the methods class to get access to its methods
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.Clear(); //required to make entire background the color
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine(@"                             .\^/.          ");
+            Console.WriteLine(@"                           . |`|/| .        ");
+            Console.WriteLine(@"                           |\|\|'|/|        ");
+            Console.WriteLine(@"                        .--'-\`|/-''--.     ");
+            Console.WriteLine(@"                         \`-._\|./.- '/     ");
+            Console.WriteLine(@"                         >`-._ |/.- '<      ");
+            Console.WriteLine(@"                         '~|/~~|~~\|~'      ");
+            Console.WriteLine(@"                               |            ");
+
+            Console.WriteLine("Welcome to Emeraldine Woodleave's Fantasy Character Name Generator!");
+            Task.Delay(5000); //delays the script for 5 seconds in btwn so they don't appear all at once
+            Console.WriteLine("");
+            Console.WriteLine("To get started, please select the gender you'd like the character name to be:");
+            Console.WriteLine("Enter 'F' for female, 'M' for male, 'N' for neutral, or enter 'Q' to quit.");
+
+            string genderInput = "";
+            while (genderInput.ToUpper() != "Q")
+            {
+                genderInput = Console.ReadLine();
+
+                if (genderInput.ToUpper() == "F" || genderInput.ToUpper() == "M" || genderInput.ToUpper() == "N")
+                {
+                    break; //break out of while loop & move on to next question & user input
+                }
+                else if(genderInput.ToUpper() == "Q")
+                {
+                    Environment.Exit(0); //exit the console application
+                }
+                else 
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("**Invalid option. Please enter 'F', 'M', or 'N'. Or you can enter 'Q' to Quit.**");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you.");
+            Console.WriteLine("");
+            Console.WriteLine("Please select the color of the cloak...er, I mean shirt...that you're wearing today.");
+            fantasyMethods.PrintColorOptions(); //loops through & prints each color & corresponding # choice
+            string colorInput = "";
+            int numberOfColor = -1;
+            while (colorInput.ToUpper() != "Q")
+            {
+                colorInput = Console.ReadLine();
+                try
+                {
+                    numberOfColor = int.Parse(colorInput);
+                    break; //break out of while loop & move on to next question & user input
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("**Invalid option. Please enter a number from the list corresponding to your selected color.**");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+            if(colorInput == "Q")
+            {
+                Environment.Exit(0); //exit the console application
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Thank you.");
+            Console.WriteLine();
+            Console.WriteLine("Please select your favorite mythical creature from below.");
+            Console.WriteLine("This list is nowhere near all inclusive, so I apologize if your familiar is not in the list;"); 
+            Console.WriteLine("Emeralidine loves and supports all fantastical animals!");
+            Console.WriteLine();
+            fantasyMethods.PrintCreatureOptions();
+            string creatureInput = "";
+            int numberOfCreature = -1;
+            while (creatureInput.ToUpper() != "Q")
+            {
+                creatureInput = Console.ReadLine();
+                try
+                {
+                    numberOfCreature = int.Parse(creatureInput);
+                    break; //break out of while loop & move on to next action
+                }
+                catch
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkRed;
+                    Console.WriteLine("**Invalid option. Please enter a number from the list corresponding to your selected creature.**");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                }
+            }
+
+            if (creatureInput == "Q")
+            {
+                Environment.Exit(0); //exit the console application
+            }
+
+            //takes above inputs and passes them into the methods to get first & last name based on user's selections
+            string characterFirstName = fantasyMethods.GetFirstName(genderInput.ToUpper(), numberOfColor);
+            string characterLastName = fantasyMethods.GetLastName(genderInput.ToUpper(), numberOfCreature);
+
+            //convert gender letter to full word for retur message printed out below
+            string characterGenderFullWord = "";
+            if (genderInput == "F")
+            {
+                //select corresponding name from femaleFirstNames as per the user input from {above}-should be static accessed via array index[0]
+                characterGenderFullWord = "female";
+            }
+            else if (genderInput == "M")
+            {
+                // maleFirstNames
+                characterGenderFullWord = "male";
+            }
+            else if (genderInput == "N")
+            {
+                // neutralFirstNames
+                characterGenderFullWord = "neutral";
+            }
+
+
+            Console.WriteLine("Wonderful, thank you kindly. Give the computer one moment to peruse the fantasy worlds.");
+            Task.Delay(3000); //delays the script for 3 seconds
+            Console.WriteLine("In the meantime, here's a joke to make you chuckle: Why does the dragon only sleep during the day?...");
+            Task.Delay(7000); //delays the script for 7 seconds
+            Console.WriteLine("Because it hunts knights! :D ");
+
+            Console.WriteLine($"Your new {characterGenderFullWord} fantasy character name is: {characterFirstName} {characterLastName}");
+            Task.Delay(3000);//delays the script for 3 seconds
+            Console.WriteLine("Thank you for playing!");
+            Console.WriteLine("If you'd like to generate another name, hit Enter. Otherwise, you can hit 'Q' to quit.");
+            Console.Clear();
+
+
+        }  //end of static main
+    }
+}
